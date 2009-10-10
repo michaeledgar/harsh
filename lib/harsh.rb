49cb941ffd896c3481508bd678e040a0012aa334
Michael Edgar <michael.j.edgar@dartmouth.edu>
2009-10-09 20:30:06 -0400

    * refactored HAML to be a real module (not one created by eval()) * refactored argument parsing for ERb style invocation * code prettified

--- lib/harsh.rb
+++ lib/harsh.rb
@@ -1,72 +1,76 @@
 # Harsh
+
+module ::Haml
+  module Filters
+    module Harsh
+      def initialize(text)
+        @text = highlight_text(text)
+      end
+      def highlight_text(text, opts = ::Harsh::DEFAULT_OPTIONS)
+        Uv.parse( text, "xhtml", opts[:format], opts[:lines], opts[:theme])
+      end
+      def parse_opts(text)
+        opts = {}
+        all_lines = text.split(/\n/)
+        return [opts, text] unless all_lines.first =~ /#!harsh/
+        
+        line = all_lines.first
+        opts[:format] = $1 if line =~ /syntax\s*=\s*([\w-]+)/
+        opts[:theme]  = $1 if line =~ /theme\s*=\s*([\w-]+)/
+        opts[:lines]  = (line =~ /lines\s*=\s*(\w+)/) ? ($1 == 'true') : false
+        
+        [opts, all_lines[1..-1].join("\n")]
+      end
+      
+      def render(text)
+        opts, text = parse_opts(text)
+        Haml::Helpers.preserve(highlight_text(text.rstrip, ::Harsh::DEFAULT_OPTIONS.merge(opts)))
+      end
+    end
+  end
+end
+  
 module Harsh
   module ErbMethods
     def syntax_highlight(*args, &block)
-      require 'uv'
-      default_options = {:format => "ruby", :theme => "twilight", :lines => false}
-      case args.size
-      when 0
-        opts = default_options
-      when 1
-        if args[0].is_a? String
-          text = args[0]
-          opts = default_options
-        elsif args[0].is_a? Hash
-          opts = default_options.merge(args[0])
-        else
-          raise ArgumentError.new("Must pass either a string or a hash to syntax_highlight()/harsh()")
-        end
-      when 2
-        text = args[0]
-        text = text.sub(/\n/,"") if text =~ /^(\s*)\n/
-        opts = default_options.merge(args[1])
-      end
+      require 'uv' unless defined? Uv
+      opts, text = Harsh::parse_args(args)
       if block_given?
         text = capture(&block)
         text = text.sub(/\n/,"") if text =~ /^(\s*)\n/
-        concat(Uv.parse( text, "xhtml", opts[:format].to_s, opts[:lines], opts[:theme].to_s))
-        ""
-      else
-        concat(Uv.parse( text, "xhtml", opts[:format].to_s, opts[:lines], opts[:theme].to_s))
-        ""
       end
+      concat(Uv.parse( text, "xhtml", opts[:format].to_s, opts[:lines], opts[:theme].to_s))
+      ""
     end
     alias_method :harsh, :syntax_highlight
   end
   
-  def enable_haml
-    eval <<-EOF
-      module ::Haml
-        module Filters
-          module Harsh
-            include Haml::Filters::Base
-            lazy_require 'uv'
-            def initialize(text)
-              @text = highlight_text(text)
-            end
-            def highlight_text(text, opts = {:format => "ruby", :theme => "twilight", :lines => false})
-              Uv.parse( text, "xhtml", opts[:format], opts[:lines], opts[:theme])
-            end
-            def render(text)
-              all_lines = text.split(/\\n/)
-              if all_lines.first =~ /#!harsh/
-                line = all_lines.first
-                syntax = (line =~ /syntax\\s*=\\s*([\\w-]+)/) ? $1 : "ruby"
-                theme = (line =~ /theme\\s*=\\s*(\\w+)/) ? $1 : "twilight"
-                lines = (line =~ /lines\\s*=\\s*(\\w+)/) ? ($1 == 'true') : false
-                text = all_lines[1..-1].join("\\n")
-                puts({:format => syntax, :theme => theme, :lines => lines}.inspect)
-                Haml::Helpers.preserve(highlight_text(text.rstrip, :format => syntax, :theme => theme, :lines => lines))
-              else
-                Haml::Helpers.preserve(highlight_text(text.rstrip))
-              end
-            end
-          end
-        end
+  class << self
+    def enable_haml
+      ::Haml::Filters::Harsh.send(:include, Haml::Filters::Base)
+      ::Haml::Filters::Harsh.module_eval do
+        lazy_require 'uv'
       end
-    EOF
+    end
+    
+    def defaults(settings={})
+      settings.each do |k, v|
+        Harsh::DEFAULT_OPTIONS[k] = v
+      end
+    end
   end
-  module_function :enable_haml
+  
+  private
+  
+  DEFAULT_OPTIONS = {:format => "ruby", :theme => "twilight", :lines => false}
+  
+  def self.parse_args(args)
+    return DEFAULT_OPTIONS if args.empty?
+    
+    text = args.first.is_a?(String) ? args.first : nil
+    opts = args.last.is_a?(String) ? DEFAULT_OPTIONS : DEFAULT_OPTIONS.merge(args.last)
+    text = text.sub(/\n/,"") if text =~ /^(\s*)\n/
+    
+    [opts, text]
+  end
 end
-
-
