27bf051fe1ea68f683960e7a364145b5f6620fd2
Evan Sagge <evansagge@gmail.com>
2010-04-06 22:36:52 -0400

    Fixed RAILS_ROOT dependency.

--- init.rb
+++ init.rb
@@ -3,5 +3,5 @@ require File.dirname(__FILE__)+"/lib/harsh.rb"
 ActionView::Base.send(:include, Harsh::ErbMethods)
 
 
-harsh_sheets = Dir.entries(RAILS_ROOT+"/public/stylesheets/harsh/").reject {|s| s =~ /^\./}.map {|s| "harsh/"+s}
+harsh_sheets = Dir.entries(Rails.root.join('public', 'stylesheets', 'harsh')).reject {|s| s =~ /^\./}.map {|s| "harsh/"+s}
 ActionView::Helpers::AssetTagHelper.register_stylesheet_expansion :harsh => harsh_sheets
